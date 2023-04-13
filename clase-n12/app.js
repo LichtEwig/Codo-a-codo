e5b347bcef30d482da0b70781b821987cd4964f2
LichtEwig <lichtewig73@gmail.com>
2023-04-13 17:25:53 -0300

    clase12

---
+++ clase-n12/app.js
@@ -0,0 +1,15 @@
+let entrada = prompt('Ingresa tu nombre: ');
+
+alert('Bienvenida ' + entrada);
+
+console.log(entrada);
+
+let a = 'abc';
+console.log(a);
+a= 123;
+console.log(a);
+
+const tus ='dft';
+console.log(tus);
+var ab= 123;
+console.log(ab);
